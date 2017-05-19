7e7d295cbbb7bcf0985a0e538820daac4f623baf
Garrett Kucinski <garrettryan@doejo.com>
2017-05-19 15:49:01 -0500

    condensing to one file

--- js/src/player.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    "use strict";
-
-    const Player = function() {
-
-    };
-}());
---
+++ js/tic-tac-toe.ts
@@ -0,0 +1,28 @@
+"use strict";
+
+var TicTacToe = TicTacToe || {};
+
+TicTacToe.board = (function() {
+    const squares = [];
+
+    const add = (square) => {
+        squares.push(square);
+    };
+
+    const viewArray = () => {
+        console.log(squares);
+    };
+
+    return {
+        viewArray: viewArray,
+        add: add
+    };
+})();
+
+TicTacToe.square = (function() {
+    // Some properties and methods go here
+})();
+
+TicTacToe.board.add('hello');
+TicTacToe.board.add('World');
+TicTacToe.board.viewArray();
